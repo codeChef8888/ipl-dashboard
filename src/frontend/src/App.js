e54bb10a6500b9d7053846c6da893c323b363287
Arya555 <monkey.dstrawhat77@gmail.com>
2021-11-08 23:34:24 +0545

    Team Page : Completed UI Elements for

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { TeamPage } from './pages/TeamPage';
-import NotFound from './pages/NotFound';
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
     <Router>
       <div className="App">
 
-        <Route exact path="/team/:teamName">
-          <TeamPage />
-        </Route>
+
+        <Switch>
+
+          <Route path="/team/:teamName/matches/:year">
+            <MatchPage />
+          </Route>
+
+          <Route path="/team/:teamName">
+            <TeamPage />
+          </Route>
+          
+        </Switch>
 
       </div>
     </Router>
---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+export const MatchPage = () => {
+    return (  
+        <div className="MatchPage">
+            <h1>Match Page.</h1>
+        </div>
+    );
+}
+ 
