ffda532ea54a27913ad1aa41aed7136c74296322
Arya555 <monkey.dstrawhat77@gmail.com>
2021-11-09 09:16:12 +0545

    Added Backend Match API

--- src/main/java/io/codechef/ipldashboard/controller/TeamController.java
+++ src/main/java/io/codechef/ipldashboard/controller/TeamController.java
@@ -1,10 +1,15 @@
 package io.codechef.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.codechef.ipldashboard.model.Match;
 import io.codechef.ipldashboard.model.Team;
 import io.codechef.ipldashboard.repository.TeamRepository;
 import io.codechef.ipldashboard.repository.MatchRepository;
@@ -13,9 +18,8 @@ import io.codechef.ipldashboard.repository.MatchRepository;
 @CrossOrigin
 public class TeamController {
 
-    private TeamRepository teamRepository;  
+    private TeamRepository teamRepository;
     private MatchRepository matchRepository;
-
 
     public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
         this.teamRepository = teamRepository;
@@ -24,11 +28,18 @@ public class TeamController {
 
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName) {
-        Team team= this.teamRepository.findByTeamName(teamName);
-        System.out.println(team);
+        Team team = this.teamRepository.findByTeamName(teamName);
         team.setMatches(matchRepository.findLatestMatchesbyTeam(teamName, 4));
-        System.out.println(team.getTotalMatches());
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+
+        return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
     }
 
 }
