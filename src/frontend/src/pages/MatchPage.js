8540c7fd0b58f55418c5d6adc6c9270717d53abc
Arya555 <monkey.dstrawhat77@gmail.com>
2021-11-09 09:37:36 +0545

    Added MatchPage UI

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { MatchDetailCard } from "../components/MatchDetailCard";
+
 export const MatchPage = () => {
-    return (  
+    const [matches, setMatches] = useState([]);
+    const { teamName, year } = useParams();
+
+    useEffect(() => {
+        const fetchMatches = async () => {
+            const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+            const data = await response.json();
+            setMatches(data);
+            console.log(data);
+        }
+        fetchMatches();
+    }, [])
+
+    return (
         <div className="MatchPage">
             <h1>Match Page.</h1>
+            {
+                matches.map(match => <MatchDetailCard teamName={teamName} match={match} />)
+            }
         </div>
     );
 }
- 
+
